2c083e2cf4918a133ca894da5c33656f6e923767
adrianccrs <yeadcato@gmail.com>
2025-06-28 18:21:30 -0400

    DefaultLayout created

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@components/Navbar";
+import { useAuth } from "@context/AuthContext";
+import { Outlet } from "react-router-dom";
+
+export default function DefaultLayout() {
+    const auth = useAuth();
+
+    return (
+        <>
+        <Navbar username={`${auth.customUser?.firstName || ""} ${auth.customUser?.secondName || ""} ${auth.customUser?.firstLastName || ""} ${auth.customUser?.secondLastName}`} />
+        <main className={`flex flex-1 min-w-0 justify-center items-center min-h-screen w-full px-10 py-12 bg-background}`}>
+            <Outlet/>
+        </main>
+        </>
+    );
+}
