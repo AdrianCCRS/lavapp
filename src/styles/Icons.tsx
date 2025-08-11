f13df5e57405779df7c9ef7aa8917837961b149e
AdrianCCRS <yeadcato@gmail.com>
2025-08-11 15:49:49 -0500

    feat(ui) Application logo implemented to imports

---
+++ src/styles/Icons.tsx
@@ -0,0 +1,37 @@
+import type {SVGProps} from "react";
+
+export type IconSvgProps = SVGProps<SVGSVGElement> & {
+    size?: number;
+  };
+
+export const LogoutIcon = ({fill = "currentColor", size, height, width, ...props}: IconSvgProps) => {
+    return (
+        <svg 
+            width={size || width} 
+            height={size || height} 
+            viewBox="0 0 24 24" 
+            xmlns="http://www.w3.org/2000/svg" 
+            fill="none" 
+            {...props}
+        >
+            <path 
+                d="M9.00195 7C9.01406 4.82497 9.11051 3.64706 9.87889 2.87868C10.7576 2 12.1718 2 15.0002 2L16.0002 2C18.8286 2 20.2429 2 21.1215 2.87868C22.0002 3.75736 22.0002 5.17157 22.0002 8L22.0002 16C22.0002 18.8284 22.0002 20.2426 21.1215 21.1213C20.2429 22 18.8286 22 16.0002 22H15.0002C12.1718 22 10.7576 22 9.87889 21.1213C9.11051 20.3529 9.01406 19.175 9.00195 17" 
+                stroke={fill} 
+                stroke-width="1.5" 
+                stroke-linecap="round"
+            />
+            <path 
+                d="M15 12L2 12M2 12L5.5 9M2 12L5.5 15" 
+                stroke={fill} 
+                stroke-width="1.5" 
+                stroke-linecap="round" 
+                stroke-linejoin="round"
+            />
+        </svg>
+    );
+};
+export const LavAppLogo = ({size = 24}) => {
+    return (
+     <img src="/assets/lavapp-logo-v2-nobg.png" width={size} height={size} alt="LavApp Logo"/>
+    );
+}
